945a5b495b4812c22d3333d5b7edf6bc31092f51
Seagle27 <tomershiran98@gmail.com>
2020-12-19 21:14:05 +0200

    Jack Tokenizer WIP

--- projects/10/ syntax_analysis/jack_tokenizer.py
+++ projects/10/ syntax_analysis/jack_tokenizer.py
@@ -1,11 +1,16 @@
 from enum import IntEnum
+from collections import deque
+from functools import reduce
+import re
+from typing import List
 
 
 class TokenTypes(IntEnum):
-    KEYWORD_SYMBOL = 0
-    IDENTIFIER = 1
-    INT_CONST = 2
-    STRING_CONST = 3
+    KEYWORD = 0
+    SYMBOL = 1
+    IDENTIFIER = 2
+    INT_CONST = 3
+    STRING_CONST = 4
 
 
 class Keyword(IntEnum):
@@ -36,15 +41,23 @@ class JackTokenizer:
     """
     Ignores all comments and white space in the input stream, and serializes it into jack-language tokens.
     """
+    JACK_SYMBOLS = {'{', '}', '(', ')', '[', ']', '.', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~'}
+    JACK_KEYWORDS = {'CLASS', 'CONSTRUCTOR', 'FUNCTION', 'METHOD',
+                     'FIELD', 'STATIC', 'VAR', 'INT', 'CHAR', 'BOOLEAN',
+                     'VOID', 'TRUE', 'FALSE', 'NULL', 'THIS', 'LET', 'DO',
+                     'IF', 'ELSE', 'WHILE', 'RETURN'}
+
     def __init__(self, jack_file_path: str):
-        self.jack_file_stream = open(jack_file_path, 'r')
+        self.jack_file_tokens = self._load_file(jack_file_path)
+        self._token_idx = 0
+        self.current_token = None
 
     def has_more_tokens(self) -> bool:
         """
         Are there more tokens in the input?
         :return: bool.
         """
-        pass
+        return len(self.jack_file_tokens) > self._token_idx
 
     def advance(self) -> None:
         """
@@ -53,15 +66,25 @@ class JackTokenizer:
         Initially there is no current token.
         :return: None.
         """
-        pass
+        self.current_token = self.jack_file_tokens[self._token_idx]
+        self._token_idx += 1
 
     def token_type(self) -> TokenTypes:
         """
         :return: The type of the current token, as a Enum.
         """
-        pass
+        if self.current_token.upper() in self.JACK_KEYWORDS:
+            return TokenTypes.KEYWORD
+        elif self.current_token in self.JACK_SYMBOLS:
+            return TokenTypes.SYMBOL
+        elif self.current_token.isdigit() and int(self.current_token) <= 32767:
+            return TokenTypes.INT_CONST
+        elif self.current_token.startswith('"') and self.current_token.endswith('"'):
+            return TokenTypes.STRING_CONST
+        else:
+            return TokenTypes.IDENTIFIER
 
-    def keyword(self) -> Keyword:
+    def key_word(self) -> Keyword:
         """
         This method should be called only if token_type is KEYWORD.
         :return: The Keyword which is the current token as a Enum.
@@ -75,6 +98,29 @@ class JackTokenizer:
         """
         pass
 
+    def identifier(self) -> str:
+        """
+        This method should be called only if token_type is IDENTIFIER.
+        :return: The identifier which is the current token
+        """
+        pass
+
     @staticmethod
-    def _remove_whitespace_and_comments(line) -> str:
-        return line.strip().split("//")[0].strip()
+    def _load_file(file_path: str) -> List[str]:
+        """
+        Prepares the data for the JackTokenizer, in an easier to work with format.
+        Reads the file, removes all the comments, whitespaces.
+        :param file_path: File path to work on.
+        :return: A list with all the file components.
+        """
+        with open(file_path, 'r') as f:
+            file_content = f.read()
+
+        str_without_comments = re.sub('\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$', '', file_content, flags=re.MULTILINE)
+        str_without_new_lines = str_without_comments.lstrip('\n').replace('\n', ' ')
+        return JackTokenizer._split_keep_seperators(str_without_new_lines)
+
+    @staticmethod
+    def _split_keep_seperators(text: str) -> List[str]:
+        all_matches = re.split('([{}\(\)\[\]\.,;\*&\|<>=~\+-\/ ])', text)
+        return [curr_symbol for curr_symbol in all_matches if curr_symbol and curr_symbol != ' ']
