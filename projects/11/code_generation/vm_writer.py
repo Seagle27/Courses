507b1fd88ce88f52118f12840241afecf148212e
Seagle27 <tomershiran98@gmail.com>
2021-01-09 15:42:38 +0200

    Finish vm_writer

--- projects/11/code_generation/vm_writer.py
+++ projects/11/code_generation/vm_writer.py
@@ -6,62 +6,74 @@ class VMWriter:
         Creates a new output .vm file and prepares it for writing.
         """
         self.output = open(output_file_path, 'w')
+        self._memory_segments = {"LCL": "local", "ARG": "argument", "CONST": "constant", "FIELD": "this"}
+        self._memory_segments.update({key: key.lower() for key in ['POINTER', 'STATIC', 'THIS', 'THAT', 'TEMP']})
 
     def write_push(self, segment: str, index: int) -> None:
         """
         Writes a vm push command
         """
-        pass
+        segment = self._memory_segments[segment]
+        self._write(f'push {segment} {index}')
 
     def write_pop(self, segment: str, index: int) -> None:
         """
         Writes a vm pop command
         """
-        pass
+        segment = self._memory_segments[segment]
+        self._write(f'pop {segment} {index}')
 
-    def write_aritmetic(self, command: str) -> None:
+    def write_arithmetic(self, command: str) -> None:
         """
         Writes a VM arithmetic-logical command
         """
+        if command not in ('ADD', 'SUB', 'NEG', 'EQ', 'GT',
+                           'LT', 'AND', 'OR', 'NOT'):
+            raise TypeError(f'{command} not supported.')
+
+        self._write(command.lower())
 
     def write_label(self, label: str) -> None:
         """
         Writes a VM label command
         """
-        pass
+        self._write(f'label {label}')
 
     def write_goto(self, label: str) -> None:
         """
         Writes a VM goto command
         """
-        pass
+        self._write(f'goto {label}')
 
     def write_if(self, label: str) -> None:
         """
         Writes a VM if-goto command
         """
-        pass
+        self._write(f'if-goto {label}')
 
     def write_call(self, name: str, number_args: int) -> None:
         """
         Writes a VM call command
         """
-        pass
+        self._write(f'call {name} {str(number_args)}')
 
     def write_function(self, name: str, number_locals: int) -> None:
         """
         Writes a VM function command
         """
-        pass
+        self._write(f'function {name} {str(number_locals)}')
 
     def write_return(self) -> None:
         """
         Write a VM return command
         """
-        pass
+        self._write('return')
 
     def close(self) -> None:
         """
         Closes the output file.
         """
         self.output.close()
+
+    def _write(self, cmd: str):
+        self.output.write(cmd+'\n')
